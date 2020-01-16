47b7b3cc41ff32274c87675c5c275e4abe6dc9a8
David I. Lehn <dlehn@digitalbazaar.com>
2020-01-16 16:25:45 -0500

    Add initial files.

---
+++ BrQTable.vue
@@ -0,0 +1,15 @@
+<template>
+</template>
+<script>
+/*!
+ * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+export default {
+  name: 'BrQTable',
+  props: {}
+};
+</script>
+<style lang="scss">
+</style>
---
+++ README.md
@@ -0,0 +1,3 @@
+# bedrock-quasar-table
+
+A Quasar Table component for Bedrock Web apps.
